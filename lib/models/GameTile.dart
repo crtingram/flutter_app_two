25eb7c6f793ce255c40ae0f92c744596dc089eb2
Cory Ingram <cory.ingram@qhrtech.com>
2019-04-04 15:39:15 -0700

    Create GameTile.dart

---
+++ lib/models/GameTile.dart
@@ -0,0 +1,23 @@
+class GameTile {
+  GameTileType type;
+  List<GameTileContentType> contents;
+
+  GameTile({GameTileType type, List<GameTileContentType> contents})
+      : assert(contents.length < 4);
+
+  GameTile.grassTile({List<GameTileContentType> contents})
+      : this(type: GameTileType.grass, contents: contents);
+
+  GameTile.grassTileEmpty()
+      : this(type: GameTileType.grass, contents: [GameTileContentType.empty]);
+
+  GameTile.waterTile()
+      : this(type: GameTileType.water, contents: [GameTileContentType.water]);
+
+  GameTile.rockTile()
+      : this(type: GameTileType.rock, contents: [GameTileContentType.rock]);
+}
+
+enum GameTileType { grass, rock, water }
+
+enum GameTileContentType { enemy, chest, empty, item, rock, water }
